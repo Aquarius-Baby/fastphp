66996a574f570bc784fbb44f6a83d5603860f019
Aquarius <chenmengyan93@163.com>
2020-12-24 13:53:09 +0800

    demo 初始化

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+//
+//// 数据库配置
+$config['db']['host'] = 'localhost:3306';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'cmy123';
+$config['db']['dbname'] = 'php';
+//
+//// 默认控制器和操作名
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
